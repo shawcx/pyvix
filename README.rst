2c310be16d975d770a50cc7cc216e2c46e078305
Matthew Oertle <moertle@gmail.com>
2017-03-29 23:15:29 -0400

    hack to compile clean on os x

--- README.rst
+++ README.rst
@@ -3,16 +3,3 @@ PyVIX
 =====
 
 Python bindings for VMware products.
-
-On macOS you may receive the following error:
-::
-    import pyvix
-    Traceback (most recent call last):
-      File "<stdin>", line 1, in <module>
-    ImportError: dlopen(/Library/Frameworks/Python.framework/Versions/3.5/lib/python3.5/site-packages/pyvix.cpython-35m-darwin.so, 2): Library not loaded: libvixAllProducts.dylib
-      Referenced from: /Library/Frameworks/Python.framework/Versions/3.5/lib/python3.5/site-packages/pyvix.cpython-35m-darwin.so
-      Reason: image not found
-
-It can be fixed by running the following command:
-::
-    $ install_name_tool -change libvixAllProducts.dylib /Applications/VMware\ Fusion.app/Contents/Public/libvixAllProducts.dylib /path/to/python3.5/site-packages/pyvix.cpython-35m-darwin.so
--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import os
 import platform
 
 import setuptools
+from setuptools.command.build_ext import build_ext as _build_ext
 
 if 'Windows' == platform.system():
     if 'AMD64' == platform.machine():
@@ -18,21 +19,35 @@ if 'Windows' == platform.system():
     include_dirs = [vixpath]
     library_dirs = [vixpath]
 elif 'Darwin' == platform.system():
+    VIX_PATH = '/Applications/VMware Fusion.app/Contents/Public'
+    VIX_LIB = 'vixAllProducts'
     defines = []
-    include_dirs = ['/Applications/VMware Fusion.app/Contents/Public/include']
-    library_dirs = ['/Applications/VMware Fusion.app/Contents/Public']
-    libs = ['vixAllProducts', 'dl']
-else:
+    include_dirs = [VIX_PATH + '/include']
+    library_dirs = [VIX_PATH]
+    libs = [VIX_LIB]
+elif 'Linux' == platform.system():
     defines = []
     include_dirs = ['/usr/include/vmware-vix']
     library_dirs = ['/usr/lib/vmware-vix/lib']
     libs = ['vixAllProducts', 'dl']
 
+
+# hack to run install_name_tool to fix path to vix library
+class build_ext(_build_ext):
+    def build_extension(self, ext):
+        _build_ext.build_extension(self, ext)
+        if 'Darwin' == platform.system():
+            ext_name = self.get_ext_fullpath(ext.name)
+            fmt = 'install_name_tool -change lib%s.dylib "%s/lib%s.dylib" %s'
+            os.system(fmt % (VIX_LIB, VIX_PATH, VIX_LIB, ext_name))
+
+
 setuptools.setup(
+    cmdclass = {'build_ext': build_ext},
     name             = 'pyvix',
     author           = 'Matthew Oertle',
     author_email     = 'moertle@gmail.com',
-    version          = '0.3',
+    version          = '0.4',
     license          = 'MIT',
     url              = 'https://github.com/moertle/pyvix',
     description      = 'Python bindings for VIX library.',
@@ -41,10 +56,10 @@ setuptools.setup(
         setuptools.Extension(
             'pyvix',
             ['src/pyvix.c', 'src/pyvix_host.c', 'src/pyvix_vm.c'],
-            define_macros = defines,
-            include_dirs  = include_dirs,
-            library_dirs  = library_dirs,
-            libraries     = libs
+            define_macros   = defines,
+            include_dirs    = include_dirs,
+            library_dirs    = library_dirs,
+            libraries       = libs,
             )
         ],
     classifiers=[
